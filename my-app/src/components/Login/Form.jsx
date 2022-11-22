f86cd00bea502e5d913602d9389d887d52764e23
gabri <gchorcasitas@gmail.com>
2022-11-22 16:18:22 -0600

    feature: mock login

--- my-app/src/components/Login/Form.jsx
+++ my-app/src/components/Login/Form.jsx
@@ -1,16 +1,46 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
 function Form () {
 let navigate = useNavigate();
   
-  const [text, setText] = useState('');
-  const handleInputChange = ({target}) => {
-    setText(target.value)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [login, setLogin] = useState(false);
+
+  const handleEmailChange = ({target}) => {
+    setEmail(target.value)
   }
+
+  const handlePasswordChange = ({target}) => {
+    setPassword(target.value)
+  }
+
   const handleSubmit= (e) => {
     e.preventDefault();
-    navigate("/waiters");
-    console.log(text);
+    axios.get('https://6372d80a348e947299fdd17b.mockapi.io/users').then((result) => {
+      result.data.forEach((user) => {
+          if (user.password === password && user.email === email) {
+            const role = user.role;
+            switch(role){
+              case('admin'):
+                console.log('admin');
+                break;
+              case('kitchen'):
+                console.log('kitchen');
+                break;
+              case('waiter'):
+                navigate('/waiters');
+                break;
+              default:
+                console.log('no estas registrado');
+                break;
+            }
+        } else {
+          console.log('no estás registradox2')
+        }
+      })
+    })
   }
     return (
         <form className="form-box" onSubmit={handleSubmit} >
@@ -19,14 +49,15 @@ let navigate = useNavigate();
                 <input
                     className="input-form"
                     placeholder="email"
-                    value={text} onChange={handleInputChange}
+                    value={email} onChange={handleEmailChange}
                     type="text"
                 />
             <span className="fields-form">Password</span>
                 <input
                     className="input-form"
                     placeholder="password"
-                    type="text"
+                    value={password} onChange={handlePasswordChange}
+                    type="password"
                 />
          <button id="start"  type= 'submit'>Start</button>
         </form>
