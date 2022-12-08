01cd79b857313e0b26fac3a37836404af1930627
gabri <gchorcasitas@gmail.com>
2022-12-08 13:12:09 -0600

    hotfix: automatic update of kitchen orders

--- my-app/src/components/Kitchen/Kitchen.jsx
+++ my-app/src/components/Kitchen/Kitchen.jsx
@@ -6,6 +6,7 @@ import {useLoaderData} from "react-router-dom";
 import ModalConfirmKitchen from "./KitchenModals/ModalConfirmKitchen";
 import {useState} from 'react';
 import axios from 'axios';
+import getData from "../../getData";
 
 function Kitchen(){
     const[isOrderReady, openOrderReady, closeOrderReady]=useModal(false);
@@ -23,8 +24,10 @@ function Kitchen(){
   function editState(){
     const urlOrders = `https://6372d80a348e947299fdd17b.mockapi.io/orders/${idOrder}`;
     axios.put(urlOrders, {...editOrder, status : 'done'})
-    .then((result) => {
-      closeOrderReady()
+    .then(async (result) => {
+        const dataProducts = await getData("https://6372d80a348e947299fdd17b.mockapi.io/orders/");
+        closeOrderReady()
+        return setOrderReady(dataProducts)
     })
    }
 
