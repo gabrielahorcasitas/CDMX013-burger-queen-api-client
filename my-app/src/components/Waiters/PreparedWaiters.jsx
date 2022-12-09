37634f7d61b0c6f0b00f5dd4b6994f3595d72e39
gabri <gchorcasitas@gmail.com>
2022-12-08 19:03:27 -0600

    chore: add style to done orders in waiters layout

--- my-app/src/components/Waiters/PreparedWaiters.jsx
+++ my-app/src/components/Waiters/PreparedWaiters.jsx
@@ -1,18 +1,18 @@
 function PreparedWaiters({order}) {
     const productValue= Object.values(order);
     const products = productValue[2];
-    console.log(products)
     let total = 0;
    
     let productData =[];
      products.forEach(element => {
      const itemPrice = element.product.price*element.quantity;
      total = itemPrice+total;
-      productData.push(<p className="product-qtys" key={element.id}>({element.quantity}) {element.name} {element.product.price}</p>)
+      productData.push(<div className="box-cout-product-waiters">
+        <p className="product-qtys-waiters" key={element.id}>({element.quantity}) {element.name}</p>
+        <p className="product-qtys-waiters" key={element.id}>${element.product.price}</p>
+        </div>)
      });
-  
-    const prepatationTime = ((order.dataProcessedMil-order.dataEntryMil)/60000).toFixed();
-  
+    
       return (
         <>
         <tr>
@@ -20,19 +20,15 @@ function PreparedWaiters({order}) {
           <div className="container-order-prepared">
             <div className="order-colum">
               <div className= "seccion-order">
-                  <h1 className="number-table"> Table: {order.table}</h1> 
-                  <div className="box-cout-product">
+                  <h1 className="number-table"> Table #{order.table}</h1> 
                    {productData} 
-                  </div> 
                 </div>
             </div>
-            <div className="order-colum">
-              <div className="mgs-Box">
-                <h2 className="mgs-delivered">Delivered:  {order.dataProcessedStr} </h2>
+              <div className="mgs-box-waiters">
+                <h2 className="mgs-delivered-waiters">Delivered:  {order.dataProcessedStr} </h2>
                 <h2 className="msg-total-price">Total ${total}</h2>
               </div>
            </div>
-          </div>
           </td>
         </tr>
       </>
