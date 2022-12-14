d0077d1f92ab3f0e03f1b4d94e6a80b356e16bc2
Karlacruz11 <cruz.pekarla@gmail.com>
2022-12-13 19:26:39 -0600

    chore: add useEffect in layout prepared

--- my-app/src/components/Kitchen/PreparedContainer.jsx
+++ my-app/src/components/Kitchen/PreparedContainer.jsx
@@ -3,12 +3,27 @@ import Headers from "../Headers";
 import { useModal } from "../useModal";
 import PreparedTable from "./PreparedTable";
 import ModalConfirmKitchen from "./KitchenModals/ModalConfirmKitchen";
-import {useState} from 'react';
-import {useLoaderData} from 'react-router-dom'
+import {useState, useEffect} from 'react';
+import {useLoaderData, useRevalidator} from 'react-router-dom'
 
 function PreparedContainer ({handleAccount}){
+    const data = useLoaderData();
     const[isOrderReady, openOrderReady, closeOrderReady]=useModal(false);
-    const [orderReady, setOrderReady] = useState(useLoaderData());
+    const [orderReady, setOrderReady] = useState(data);
+   const revalidator= useRevalidator();
+   
+    useEffect(() => {
+        setOrderReady(data)
+    }, [data])
+
+    useEffect(() => {
+        const id = setInterval(() => {
+            revalidator.revalidate();
+        }, 5000);
+
+        return () => clearInterval(id);
+    }, []);
+
 
     return(
     <> 
