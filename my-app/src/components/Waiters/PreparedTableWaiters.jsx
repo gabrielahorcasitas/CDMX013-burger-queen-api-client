f6c9d05d3947149f2331d9b3b86b65873b0a100b
gabri <gchorcasitas@gmail.com>
2022-12-16 22:11:50 -0600

    feature: mediaqueries waiters layouts

--- my-app/src/components/Waiters/PreparedTableWaiters.jsx
+++ my-app/src/components/Waiters/PreparedTableWaiters.jsx
@@ -2,52 +2,52 @@ import '../Kitchen/kitchenView.css'
 import burgerlogo from '../../images/burgerlogo.png';
 import PreparedWaiters from './PreparedWaiters';
 
-function  PreparedTableWaiters ({orderReady, openOrderDelivered, 
-  setIdOrder, setEditOrderDelivered }){
-  let ordersContainer = ''  ;
+function PreparedTableWaiters({ orderReady, openOrderDelivered,
+  setIdOrder, setEditOrderDelivered }) {
+  let ordersContainer = '';
 
   if (orderReady !== undefined) {
     const sentOrders = orderReady.filter((order) => order.status === 'done').sort((a, b) => a.dataProcessedMil
-    - b.dataProcessedMil
+      - b.dataProcessedMil
     );
- 
+
 
     ordersContainer = sentOrders.map((order) => {
-     return( <PreparedWaiters
-      order={order}
-      key={order.id}
-      openOrderDelivered={openOrderDelivered} 
-      setIdOrder={setIdOrder} 
-      setEditOrderDelivered={setEditOrderDelivered} 
-      
+      return (<PreparedWaiters
+        order={order}
+        key={order.id}
+        openOrderDelivered={openOrderDelivered}
+        setIdOrder={setIdOrder}
+        setEditOrderDelivered={setEditOrderDelivered}
+
 
       />)
-  })        
-};
-    return(
-        <>
-        <table className="tableOrders">
-    <thead className="table-order-header">
-      <tr>
-        <th>Orders</th>
-      </tr>
-    </thead>
-    <tbody className="order-table-body">
-      {ordersContainer}
-    </tbody>
-    <tfoot className="logo-table-box-chefs">
-      <tr className="container-logo-burger-orders">
-        <td className='container-burger-chefs'>
-            <img
-              className="burger-logo-menu" id='burger-logo-admin'
-              src={burgerlogo}
-              alt="burger-logo-menu"
-             ></img>
-        </td>
-      </tr>
-    </tfoot>
-  </table>
-  </>
+    })
+  };
+  return (
+    <>
+      <table className="table-orders-waiters">
+        <thead className="table-order-header">
+          <tr>
+            <th>Orders</th>
+          </tr>
+        </thead>
+        <tbody className="order-table-waiters-body">
+          {ordersContainer}
+        </tbody>
+        <tfoot className="logo-table-box-chefs">
+          <tr className="container-logo-burger-orders">
+            <td className='container-burger-chefs'>
+              <img
+                className="burger-logo-menu" id='burger-logo-admin'
+                src={burgerlogo}
+                alt="burger-logo-menu"
+              ></img>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </>
   )
 }
 
